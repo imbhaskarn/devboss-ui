62ddd93045ed05c2b4b8628b857a9870b2e30463
Bhaskar Sharma <imbhaskaran@gmail.com>
2023-09-29 22:49:57 +0530

    navbar

---
+++ src/pages/blog.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout/layout";
+import React from "react";
+
+const blog = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>Blog</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default blog;
